a6cb52c3cd6250219e4b48daafb3d8f1f3a6d93c
Maximo Della Maggiore <maximodellamaggiore@gmail.com>
2025-02-28 00:47:22 -0300

    Create React Query version of anecdote project 6.20 - 6.22

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,65 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests/requests'
+
+
+const App = () => {
+
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
+
+  const vote = (anecdote) => {
+    updateAnecdoteMutation.mutate( {...anecdote, votes: anecdote.votes + 1} )
+  }
+
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: 1
+  })
+
+  if (isLoading) {
+    return <div>Loading data...</div>
+  }
+
+  if (isError) {
+    return (
+    <div>Anecdotes data is unavilable
+      <p>Error: {error.message}</p>
+    </div>
+    )
+  }
+
+  const anecdotes = data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests/requests'
+
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({ 
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      console.log("Invalidating anecdotes...")
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content: content, votes: 0 })
+}
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={addAnecdote}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
